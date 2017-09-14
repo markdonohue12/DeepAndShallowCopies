99cae77fa54640dd67c9bb9fc6c2dba4f4ca0dfa
Mark Donohue <kram4md@yahoo.com>
2017-09-13 20:44:46 -0400

    second push

--- Desktop/Fall 2017/cs442/Assignments/assign_1/firstName_lastName_assign_1/myArrayList/src/myArrayList/MyArrayList.java
+++ Desktop/Fall 2017/cs442/Assignments/assign_1/firstName_lastName_assign_1/myArrayList/src/myArrayList/MyArrayList.java
@@ -1,9 +1,13 @@
 package myArrayList.driver;
 
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 public class MyArrayList
 {
+
+	private int dataArr[];	
+
 	public MyArrayList(String inFile) {
 		// Create the FileProcessor obj so I can start reading from the file
 		FileProcessor processFile = new FileProcessor(inFile);
@@ -18,18 +22,37 @@ public class MyArrayList
 		// now if openedSuccessfully, start reading lines, each time storing the return value in the array
 		if(openedSuccessfully) {
 			// while still lines left to read in input file
+			dataArr = new int[50];
 			int convertedInt = -1;
+			int index = 0;
+			int numsInserted = 0;
 			String line = processFile.readFileLine();
 			while(line != null) {
-				System.out.println("line read from file is " + line);
-				convertedInt = Integer.parseInt(line);
-				System.out.println("the INT value is " + convertedInt);
+				try {
+					convertedInt = Integer.parseInt(line);
+					numsInserted += 1;
+					if(numsInserted > dataArr.length) {
+						
+						increaseSize(dataArr);
+					}
+					dataArr[index] = convertedInt;
+				} catch (NumberFormatException e) {
+					System.err.println("File contained a non-integer line, skipped line and continued");
+				}
+				index += 1;
 				line = processFile.readFileLine();
-				// convert String to int
-				// if valid int, store it in the the array
 			}
 		}
-		
+		for(int i = 0; i < dataArr.length; i++) {
+			System.out.println("FINAL value at index " +i+" is " +dataArr[i]);
+		}
+	}
+
+	public void increaseSize(int[] dataArray) {
+		int oldLength = dataArray.length;
+		int newLength = (int)(oldLength * 1.5);
+		int newDataArr[] = Arrays.copyOf(dataArray, newLength);
+		dataArr = newDataArr;
 	}
 	
 }
