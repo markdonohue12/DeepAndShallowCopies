704b0289ddf16169c6e3dcb2eb802a4d6e6471e2
Mark Donohue <kram4md@yahoo.com>
2017-09-15 22:18:26 -0400

    added command line validation

--- Desktop/Fall 2017/cs442/Assignments/assign_1/firstName_lastName_assign_1/myArrayList/src/myArrayList/driver/Driver.java
+++ Desktop/Fall 2017/cs442/Assignments/assign_1/firstName_lastName_assign_1/myArrayList/src/myArrayList/driver/Driver.java
@@ -5,45 +5,25 @@ public class Driver
 
 	public static void main(String[] args) 
 	{
-		// pass the input file name to the MyArrayList constructor
-		MyArrayList myList = new MyArrayList(args[0]);
-		Results results = new Results();
-		// Make a myArrayListTest instance
-		MyArrayListTest myTests = new MyArrayListTest();
-		myTests.testMe(myList, results);
-		// Print Strings from Results class
-		for(int i = 0; i < results.getTestResults().length; i++) {
-			results.writeToStdOut("Test Result "+i+": " + results.getTestResults()[i]);
+		if(args.length != 3) {
+			System.err.println("Need exactly 2 arguments, program exiting");
+			System.exit(0);
+		} else {
+			// pass the input file name to the MyArrayList constructor
+			MyArrayList myList = new MyArrayList(args[0]);
+			Results results = new Results();
+			// Make a myArrayListTest instance
+			MyArrayListTest myTests = new MyArrayListTest();
+			myTests.testMe(myList, results);
+			// Print Strings from Results class
+			for(int i = 0; i < results.getTestResults().length; i++) {
+				results.writeToStdOut("Test Result "+i+": " + results.getTestResults()[i]);
+			}	
+			results.printSum(myList);
+			results.writeToFile(args[1], myList);
+			results.closeFile(args[1]);
 		}
-		results.printSum(myList);
-		results.writeToFile(args[1], myList);
-		results.closeFile(args[1]);
-		
-		
-
-	/*
-		myList.insertSorted(20);
-		myList.removeValue(8);
-		System.out.println();
-		System.out.println("the index of the first occurrence of 13 is " + myList.indexOf(13));
-		System.out.println("total num of elements in list is " + myList.size());
-		System.out.println("sum of the elements in the list is " + myList.sum());
-		myList.toString(myList.getMyListArray());
-		MyArrayListTest testing = new MyArrayListTest();
-		testing.testMe(myList);
-	*/
-		
-
-
-
-
-
-
-
-	    // command line validation is missing here. FIXME!
-		
 	}
-	
 }
 
 
