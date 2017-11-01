5597683f432e4651affb698d0d51f5d198ba6eb7
Mark Donohue <kram4md@yahoo.com>
2017-10-31 20:40:38 -0400

    first commit of assign4

---
+++ Desktop/Fall 2017/cs442/assignment4/mark_donohue_marc_suesser_assign_4/wordTree/src/wordTree/driver/Driver.java
@@ -0,0 +1,8 @@
+package wordTree.driver;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		System.out.println("build.xml file currently set up to take 1 command line argument");
+	}
+}
